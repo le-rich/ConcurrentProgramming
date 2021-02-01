59b1e887fefa0bc2a21719fa46fbd33e1c613ede
le-rich <richardle1997@gmail.com>
2021-02-01 00:44:49 -0800

    Done Question 2

--- lab2/lab2.ex
+++ lab2/lab2.ex
@@ -5,8 +5,7 @@
 defmodule Lab2 do
 # 1a
   def sin_terms(x) do
-    odds = Stream.iterate(1, fn(x) -> x + 1 end)
-    odds = Stream.filter(odds, fn(x) -> rem(x, 2) != 0 end)
+    odds = Stream.iterate(1, fn(x) -> x + 2 end)
     inversePowOdds = Stream.map(odds, fn(y) -> pow(x, y) / fact(y) end) 
     inverseNeg = Stream.map(inversePowOdds, fn(x) -> -x end) 
     inverseNeg = Stream.map_every(inverseNeg, 2, fn(x) -> -x end)
@@ -21,22 +20,54 @@ defmodule Lab2 do
   defp fact(0, acc), do: acc 
   defp fact(x, acc), do: fact(x-1, acc * x)
 
-  def primes(n), do: sieve(Enum.to_list(100_000..n), [], ceil(:math.sqrt(n)))
+  def primes(n), do: sieve(Enum.to_list(2..n), [], ceil(:math.sqrt(n)))
   defp sieve(lst, acc, sqrtVal) do
     case lst do
-      [] -> acc
+      [] -> Enum.filter(acc, fn x -> x > 999_999 end)
       [h | t] when h <= sqrtVal -> 
         sieve(Enum.filter(t, fn x -> rem(x, h) != 0 end), [h | acc], sqrtVal)
-      rest -> Enum.rverse(acc) ++ rest
+      rest -> Enum.filter((Enum.reverse(acc) ++ rest), fn x -> x > 999_999 end)
     end
   end 
   
-  def getDigits(x), do: getDigits(x, []) 
-  defp getDigits(0, acc), do: acc 
-  defp getDigits(x, acc), do: getDigits(trunc(x / 10), [rem(x, 10)] ++ acc)
 
-  def largestSixPrimePerms(lstPrimes), do: largestSixPrimePerms(lstPrimes, [])
-  defp largestSixPrimePerms(lstPrimes, acc), do: nil 
+  def getSevPrimePerms(lstPrimes), do: getSevPrimePerms(lstPrimes, [])
+  defp getSevPrimePerms([], acc), do: acc
+  defp getSevPrimePerms([h | t], acc) do
+    digitKey = Enum.sort(Integer.digits(h))
+    getSevPrimePerms(t, [{digitKey, [h]}] ++ acc)
+  end
+
+  def getLargestSevenPrimePerms(primesLst), do: mapKVP(getSevPrimePerms(primesLst), Map.new()) 
+     
+  def getMaxSevenPrimePerm(permMap), do: Enum.map(permMap, fn {_k, v} -> length(v) end) |> Enum.max()
+
+  def mapKVP(kvps, map) do
+    case kvps do
+      [] -> map
+      [{k, v} | t] -> 
+      if Map.has_key?(map, k) do
+        mapKVP(t, Map.get_and_update(map, k, fn x -> {x, x ++ v} end) |> elem(1)) 
+      else
+        mapKVP(t, Map.put(map, k, v))
+      end
+    end
+  end
+ 
+  def findKeyOrMakeNew(k, v, acc) do
+    case acc do
+      [] -> [{k, [v]}] ++ acc
+      [{headKey, headVal} | t] -> 
+        if headKey == k do 
+          [{headKey, [v] ++ headVal}] ++ acc
+        else
+          [{headKey, headVal}] ++ findKeyOrMakeNew(k, v, t)
+        end
+    end 
+  end 
+   
+
+
 end
 
 #1b
